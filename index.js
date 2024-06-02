bdcc10c81f2584ffcca74c8b9060946ec4b75922
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-06-02 11:42:36 -0700

    Step 6

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let randomNumber2 = Math.ceil(Math.random() * 6)
 
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png")
 document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png")
+
+let message = "Draw!"
+if (randomNumber1 > randomNumber2) message = "Player 1 Wins!";
+else if (randomNumber1 <randomNumber2) message = "Player 2 Wins!";
+
+document.querySelector("h1").innerText = message;
