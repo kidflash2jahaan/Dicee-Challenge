83b5972c0bd2a92ab5e13b99cb1a6886907c47ff
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-06-02 11:38:24 -0700

    Step 5

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-randomNumber1 = Math.ceil(Math.random() * 6)
+let randomNumber1 = Math.ceil(Math.random() * 6)
+let randomNumber2 = Math.ceil(Math.random() * 6)
+
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png")
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png")
